596296ae8ce5d88ee913093ad4e3bd495c0ad3de
Philip Pigmon <philippigmon@gmail.com>
2023-06-01 10:33:18 -0500

    over due push with updates

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,24 @@
 // Splide for navigation
 let splideNav = new Splide('#splide-nav', {
+    type: 'slide',
     arrows: false,
     pagination: false,
-    perPage: 6,
-    focus: 0,
     isNavigation: true,
-
+    trimSpace: true,
+    autoWidth: true,
+    mediaQuery: 'min',
+    lazyLoad: false,
 }).mount()
 
 
 // Splide for main content
 let splideMain = new Splide('#splide-main', {
-fixedHeight: "90vh",
+fixedHeight: "100vh",
 drag: true,
 rewind: true,
 type: 'slide',
+speed: 600,
+mediaQuery: 'min',
 }).mount();
 
 
